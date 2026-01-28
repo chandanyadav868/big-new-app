0176538c638957dcba21fdba2c5284933f0814cf
chandan yadav <nippiyadav890@gmail.com>
2026-01-28 17:44:24 +0530

    adding google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "News Website - Human Talking",
@@ -9,23 +10,23 @@ export const metadata: Metadata = {
         follow: true,
     },
     alternates: {
-    canonical: `${process.env.WEBSITE_URL}`,
-  },
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
-    other: [
-      {
-        rel: "mask-icon",
-        url: "/safari-pinned-tab.svg",
-        color: "#0f172a",
-      },
-    ],
-  },
-  manifest: "/site.webmanifest",
+        canonical: `${process.env.WEBSITE_URL}`,
+    },
+    icons: {
+        icon: [
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        ],
+        apple: "/apple-touch-icon.png",
+        other: [
+            {
+                rel: "mask-icon",
+                url: "/safari-pinned-tab.svg",
+                color: "#0f172a",
+            },
+        ],
+    },
+    manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -35,6 +36,17 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SBKV8PB2QN"></Script>
+                <Script>
+                    {`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-SBKV8PB2QN');
+                    `}
+                </Script>
+            </head>
             <body>
                 {children}
             </body>
