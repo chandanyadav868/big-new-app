24a1837c67b2b77fc71a539a5cd0c09e4ffbdd00
chandan yadav <nippiyadav890@gmail.com>
2026-01-28 19:20:44 +0530

    added google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,13 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* <!-- Google Tag Manager --> */}
+                <script>
+                    {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PTT8LKVV');`}
+                </script>
+                {/* <!-- End Google Tag Manager --> */}
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBKV8PB2QN"></script>
                 <script>
                     {`window.dataLayer = window.dataLayer || [];
@@ -48,6 +55,14 @@ export default function RootLayout({
                 </script>
             </head>
             <body>
+                {/* <!-- Google Tag Manager (noscript) --> */}
+                <noscript>
+                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTT8LKVV"
+                        height="0" width="0" style={{ display: "none", visibility: "hidden"}}>
+                    </iframe>
+                </noscript>
+                    {/* <!-- End Google Tag Manager (noscript) --> */ }
+
                 {children}
             </body>
         </html>
