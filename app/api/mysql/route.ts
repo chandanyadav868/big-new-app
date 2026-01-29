66bb64b24221d47b330ec8d4dfe9694c6ab78d7e
chandan yadav <nippiyadav890@gmail.com>
2026-01-29 21:41:09 +0530

    added query for mysqldb

--- app/api/mysql/route.ts
+++ app/api/mysql/route.ts
@@ -1,24 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import mysql from "mysql2/promise";
+import { db } from "@/lib/mysqldb";
 
 export async function GET(req: NextRequest) {
-
-   try {
-     const db = await mysql.createConnection({
-         host: process.env.DB_HOST,
-         port: Number(process.env.DB_PORT),
-         user: process.env.DB_USER,
-         password: process.env.DB_PASSWORD,
-         database: process.env.DB_NAME,
-     });
- 
-     console.log({db});
-
-     const [rows] = await db.query("SHOW TABLES");
-     
-     return NextResponse.json({status:200,message:"successfully",data:rows})
-   } catch (error) {
-    console.log("Error:- ", error);
-    return NextResponse.json({status:500,error:JSON.stringify((error as any).message)})
-   }
+    try {
+        const [rows] = await db.query(`SELECT * FROM wp_posts;`);
+        return NextResponse.json({ status: 200, message: "successfully", data: rows })
+    } catch (error) {
+        console.log("Error:- ", error);
+        return NextResponse.json({ status: 500, error: JSON.stringify((error as any).message) })
+    }
 }
